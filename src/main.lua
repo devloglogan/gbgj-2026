5acfe46cdf172bbe621f175b161109442260f241
MrDoctorb <zane@indybrowns.org>
2026-01-31 08:45:52 -0600

    Drawing tiles an object

--- src/main.lua
+++ src/main.lua
@@ -3,6 +3,8 @@ import("CoreLibs/ui")
 import("CoreLibs/sprites.lua")
 import("CoreLibs/object.lua")
 
+import "tile"
+
 local pd = playdate
 local gfx = playdate.graphics
 
@@ -10,8 +12,8 @@ local tileWidth = 75
 
   local noiseImages = {}
 -- 16 perlin rectangles
-local function initManualTiles()
-  for i = 0, 15 do
+local function initPerlinRectangles()
+  for i = 1, 16 do
 	  local img = gfx.image.new(tileWidth, tileWidth)
 	  gfx.pushContext(img)
 
@@ -35,6 +37,10 @@ end
 
 local function drawManualTiles()
 	-- Tiles
+	if noiseImages[0] == nil then
+		return
+	end
+
 	for i = 0, 3 do
 		for j = 0, 3 do
 			local index = j * 4 + i --+ 1
@@ -43,15 +49,27 @@ local function drawManualTiles()
 	end
 end
 
-initManualTiles()
+local tiles = {}
+local function initClassTiles()
+	for i = 0, 3 do
+		for j = 0, 3 do
+			local index = j * 4 + i + 1
+			tiles[index] = Tile(50 + (tileWidth/2)+ (i * tileWidth), -15 + (j*tileWidth) + (tileWidth/2), tileWidth, noiseImages[index])
+			tiles[index]:add()
+		end
+	end
+end
 
+
+initPerlinRectangles()
+initClassTiles()
 function pd.update()
-	gfx.clear()
-  drawManualTiles()
-
+	--gfx.clear()
+	
+	gfx.sprite.update()
+	pd.timer.updateTimers()
+	--drawManualTiles()
 	-- Top bar
 	gfx.fillRect(0, 0, 400, 30)
 
-  gfx.sprite.update()
-  pd.timer.updateTimers()
 end
--- src/tile.lua
+++ src/tile.lua
@@ -3,27 +3,12 @@ local gfx <const> = pd.graphics
 
 class('Tile').extends(gfx.sprite)
 
-local tileWidth = 75
-
-function Tile:init(x, y, zOffset)
-    self:moveTo(x,y)
+function Tile:init(x, y, tileWidth, img)
     gfx.pushContext(img)
-    local img = gfx.image.new(tileWidth, tileWidth)
-
-    for x = 0, tileWidth - 1 do
-		for y = 0, tileWidth - 1 do
-			local noiseVal = gfx.perlin(x * 0.05, y * 0.05, zOffset)
-			if noiseVal > 0.5 then
-				gfx.setColor(gfx.kColorWhite)
-			else
-				gfx.setColor(gfx.kColorBlack)
-			end
-			gfx.drawPixel(x, y)
-		end
-    end
+    self:moveTo(x,y)
     self:setImage(img)
 end
 
 function Tile:update()
-    Player.super.update(self)
+    Tile.super.update(self)
 end 
