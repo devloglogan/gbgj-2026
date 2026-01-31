4b51102c4e993c6fd93f1c0e2d23de311a189007
Logan Lang <devloglogan@gmail.com>
2026-01-30 22:12:38 -0600

    Rough blockout of tiles and top bar

--- src/main.lua
+++ src/main.lua
@@ -1,76 +1,45 @@
--- Below is a small example program where you can move a circle
--- around with the crank. You can delete everything in this file,
--- but make sure to add back in a playdate.update function since
--- one is required for every Playdate game!
--- =============================================================
+import("CoreLibs/graphics")
+import("CoreLibs/ui")
 
--- Importing libraries used for drawCircleAtPoint and crankIndicator
-import "CoreLibs/graphics"
-import "CoreLibs/ui"
-
--- Localizing commonly used globals
 local pd = playdate
 local gfx = playdate.graphics
 
--- Defining player variables
-local playerSize = 10
-local playerVelocity = 3
-local playerX, playerY = 200, 120
+local tileWidth = 75
 
--- Drawing player image
-local playerImage = gfx.image.new(32, 32)
-gfx.pushContext(playerImage)
--- Draw outline
-gfx.drawRoundRect(4, 3, 24, 26, 1)
--- Draw screen
-gfx.drawRect(7, 6, 18, 12)
--- Draw eyes
-gfx.drawLine(10, 12, 12, 10)
-gfx.drawLine(12, 10, 14, 12)
-gfx.drawLine(17, 12, 19, 10)
-gfx.drawLine(19, 10, 21, 12)
--- Draw crank
-gfx.drawRect(27, 15, 3, 9)
--- Draw A/B buttons
-gfx.drawCircleInRect(16, 20, 4, 4)
-gfx.drawCircleInRect(21, 20, 4, 4)
--- Draw D-Pad
-gfx.drawRect(8, 22, 6, 2)
-gfx.drawRect(10, 20, 2, 6)
-gfx.popContext()
+-- 16 perlin rectangles
+local noiseImages = {}
+for i = 1, 16 do
+	local img = gfx.image.new(tileWidth, tileWidth)
+	gfx.pushContext(img)
 
--- Defining helper function
-local function ring(value, min, max)
-  if (min > max) then
-    min, max = max, min
-  end
-  return min + (value - min) % (max - min)
+	local zOffset = i * 10
+	for x = 0, tileWidth - 1 do
+		for y = 0, tileWidth - 1 do
+			local noiseVal = gfx.perlin(x * 0.05, y * 0.05, zOffset)
+			if noiseVal > 0.5 then
+				gfx.setColor(gfx.kColorWhite)
+			else
+				gfx.setColor(gfx.kColorBlack)
+			end
+			gfx.drawPixel(x, y)
+		end
+	end
+
+	gfx.popContext()
+	noiseImages[i] = img
 end
 
--- playdate.update function is required in every project!
 function pd.update()
-  -- Clear screen
-  gfx.clear()
-  -- Draw crank indicator if crank is docked
-  if pd.isCrankDocked() then
-    pd.ui.crankIndicator:draw()
-  else
-    -- Calculate velocity from crank angle
-    local crankPosition = pd.getCrankPosition() - 90
-    local xVelocity = math.cos(math.rad(crankPosition)) * playerVelocity
-    local yVelocity = math.sin(math.rad(crankPosition)) * playerVelocity
-    -- Move player
-    playerX += xVelocity
-    playerY += yVelocity
-    -- Loop player position
-    playerX = ring(playerX, -playerSize, 400 + playerSize)
-    playerY = ring(playerY, -playerSize, 240 + playerSize)
-  end
-  -- Draw text
-  ---@cast playerImage -nil
-  gfx.drawTextAligned("Template configured!", 200, 30, kTextAlignment.center)
-  -- Draw player
-  if playerImage then
-    playerImage:drawAnchored(playerX, playerY, 0.5, 0.5)
-  end
+	gfx.clear()
+
+	-- Tiles
+	for i = 0, 3 do
+		for j = 0, 3 do
+			local index = j * 4 + i + 1
+			noiseImages[index]:draw(50 + (i * tileWidth), -15 + (j * tileWidth))
+		end
+	end
+
+	-- Top bar
+	gfx.fillRect(0, 0, 400, 30)
 end
